3ca422d01eb829aab1ec3e7a02a60885eb36c6b6
rushik <kuvondikovaruhshona@gmail.com>
2026-04-19 21:33:41 +0500

    fix: formatSum and TrimSpace

--- internal/product/printer.go
+++ internal/product/printer.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -22,11 +23,29 @@ const (
 ====================`
 )
 
+func formatSum(tiyins int) string {
+	sums := tiyins / 100
+	remainder := tiyins % 100
+
+	s := strconv.Itoa(sums)
+
+	var buf []byte
+	for i, ch := range s {
+		if i > 0 && (len(s)-i)%3 == 0 {
+			buf = append(buf, ' ')
+		}
+		buf = append(buf, byte(ch))
+	}
+	if remainder == 0 {
+		return string(buf)
+	}
+	return fmt.Sprintf("%s.%02d", string(buf), remainder)
+}
 func Converter(product Product, firstAmount int) string {
 	text := strings.ReplaceAll(template, "{name}", product.Name)
 	text = strings.ReplaceAll(text, "{brand}", product.Brand)
-	text = strings.ReplaceAll(text, "{price}", strconv.Itoa(product.Price))
+	text = strings.ReplaceAll(text, "{price}", formatSum(product.Price))
+	text = strings.ReplaceAll(text, "{firstAmount}", formatSum(firstAmount))
 	text = strings.ReplaceAll(text, "{inStock}", strconv.FormatBool(product.InStock))
-	text = strings.ReplaceAll(text, "{firstAmount}", strconv.Itoa(firstAmount))
 	return text
 }
